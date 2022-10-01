6ba16cf737292cdcc1383e45b3bc479c1291ce3d
dhver <dhverma@cs.stonybrook.edu>
2022-09-30 21:05:18 -0400

    Added support to save test output into files

--- transformer_test.py
+++ transformer_test.py
@@ -8,6 +8,7 @@ import util.model_utils as model_utils
 import argparse
 import util.load_utils as load_utils
 from sklearn.metrics import precision_score, recall_score, f1_score
+import os
 
 class TransformerTest():
 
@@ -17,6 +18,7 @@ class TransformerTest():
         self.test_path = options['test_path']
         self.batch_size = options['batch_size']
         self.is_hypothesis_only = options['is_hypothesis_only']
+        self.predictions_save_path = options['predictions_save_path']
         transformer = Transformer(self.model_name)
         self.model, self.tokenizer = transformer.get_model_and_tokenizer()
         self.model.to(self.device)
@@ -33,7 +35,15 @@ class TransformerTest():
         f1 = f1_score(labels_flat, pred_flat, zero_division=0)
         return acc, precision, recall, f1
 
-    def test(self, data_loader):
+    def append_predictions(self, data, indexes, preds):
+        output_shape = preds.shape[-1]
+        pred_flat = np.argmax(preds, axis=1).flatten()
+        if output_shape == 3:
+            pred_flat = np.where(pred_flat <= 1, 0, 1)
+        for index, pred in zip(indexes, pred_flat):
+            data.loc[index.item(), "prediction"] = int(pred)
+
+    def test(self, data_loader, test_df):
         self.model.eval()
         total_acc = 0
         total_loss = 0
@@ -42,7 +52,7 @@ class TransformerTest():
         total_f1 = 0
 
         with torch.no_grad():
-            for (pair_token_ids, mask_ids, seg_ids, y) in tqdm(data_loader):
+            for (pair_token_ids, mask_ids, seg_ids, indexes, y) in tqdm(data_loader):
                 # optimizer.zero_grad()
                 pair_token_ids = pair_token_ids.to(self.device)
                 mask_ids = mask_ids.to(self.device)
@@ -70,6 +80,7 @@ class TransformerTest():
                     
                     # loss = criterion(prediction, labels)
                 acc, precision, recall, f1 = self.flat_accuracy(logits, label_ids)
+                self.append_predictions(test_df, indexes, logits)
                 # print(acc, loss)
 
                 total_loss += loss.item()
@@ -92,9 +103,16 @@ class TransformerTest():
         test_df = load_utils.load_data(self.test_path)
         test_df['gold_label'] = test_df['gold_label'].astype(int)
         dataset = TransformerDatasetLoader(test_df, self.tokenizer, is_hypothesis_only=self.is_hypothesis_only)
-        data_loader = dataset.get_data_loaders(self.batch_size)
+        data_loader = dataset.get_data_loaders(self.batch_size, include_row_indexes=True)
 
-        test_acc, test_precision, test_recall, test_f1, test_loss = self.test(data_loader)
+        test_acc, test_precision, test_recall, test_f1, test_loss = self.test(data_loader, test_df)
+        column_name = "prediction"
+        test_df[column_name] = test_df[column_name].astype(int)
+        if self.predictions_save_path:
+            if not os.path.exists(self.predictions_save_path):
+                os.makedirs(os.path.dirname(self.predictions_save_path), exist_ok=True)
+                print ("Created a path: %s"%(self.predictions_save_path))
+            test_df.to_csv(self.predictions_save_path)
 
         print(f'test_acc: {test_acc:.4f} test_precision: {test_precision:.4f} test_recall: {test_recall:.4f} test_f1: {test_f1:.4f}')
 
@@ -103,6 +121,7 @@ class TransformerTest():
 
 def parse_args():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--predictions_save_path", help="Path to save the predictions")
     parser.add_argument("--test_path", help="Path to the test dataset jsonl file", default="./data/multinli_1.0/multinli_1.0_dev_mismatched.jsonl") # TODO: Add proper path
     parser.add_argument("--batch_size", help="Batch size", type=int, default=32)
     parser.add_argument("--model_name", help="Name of the huggingface model or the path to the directoy containing a pre-trained transformer", default="roberta-large-mnli")
@@ -117,6 +136,7 @@ if __name__ == '__main__':
     args = parse_args()
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # create_path(args.predictions_save_path)
     print(device)
 
     options = {}
@@ -125,6 +145,7 @@ if __name__ == '__main__':
     options['test_path'] = args.test_path
     options['model_name'] = args.model_name
     options['is_hypothesis_only'] = args.is_hypothesis_only
+    options['predictions_save_path'] = args.predictions_save_path
     print(options)
 
     tester = TransformerTest(options)
--- util/transformer_dataset_loader.py
+++ util/transformer_dataset_loader.py
@@ -12,18 +12,20 @@ class TransformerDatasetLoader(Dataset):
         self.label_dict = label_dict
         self.is_hypothesis_only = is_hypothesis_only
 
-    def process_data(self):
+    def process_data(self, include_row_indexes=False):
         print("Processing data..")
         token_ids = []
         mask_ids = []
         seg_ids = []
+        indexes = []
         y = []
 
         premise_list = self.data['sentence1'].to_list()
+        index_list = self.data.index.values.tolist()
         hypothesis_list = self.data['sentence2'].to_list()
         label_list = self.data['gold_label'].to_list()
 
-        for (premise, hypothesis, label) in tqdm(zip(premise_list, hypothesis_list, label_list), total=len(premise_list)):
+        for (premise, hypothesis, label, index) in tqdm(zip(premise_list, hypothesis_list, label_list, index_list), total=len(premise_list)):
 
             if premise != None and hypothesis != None:
                 if not self.is_hypothesis_only:
@@ -33,6 +35,7 @@ class TransformerDatasetLoader(Dataset):
                 token_ids.append(torch.tensor(encoded_values['input_ids']))
                 seg_ids.append(torch.tensor(encoded_values['token_type_ids']))
                 mask_ids.append(torch.tensor(encoded_values['attention_mask']))
+                indexes.append(index)
                 if self.label_dict != None: # In case of the RTE dataset, each column value is an integer itself. Hence, no label_dict is needed
                     y.append(self.label_dict[label])
                 else:
@@ -41,12 +44,16 @@ class TransformerDatasetLoader(Dataset):
         token_ids = pad_sequence(token_ids, batch_first=True)
         mask_ids = pad_sequence(mask_ids, batch_first=True)
         seg_ids = pad_sequence(seg_ids, batch_first=True)
+        indexes = torch.tensor(indexes)
         y = torch.tensor(y)
-        dataset = TensorDataset(token_ids, mask_ids, seg_ids, y)
+        if include_row_indexes:
+            dataset = TensorDataset(token_ids, mask_ids, seg_ids, indexes, y)
+        else:
+            dataset = TensorDataset(token_ids, mask_ids, seg_ids, y)
         return dataset
 
-    def get_data_loaders(self, batch_size=32, shuffle=True):
-        processed_dataset = self.process_data()
+    def get_data_loaders(self, batch_size=32, shuffle=True, include_row_indexes=False):
+        processed_dataset = self.process_data(include_row_indexes)
 
         data_loader = DataLoader(
             processed_dataset,
